7376071891f690e17b26d20187259ca5082f2c43
twahyuni <ti.wahyuni@gmail.com>
2016-05-31 16:39:50 +0800

    js update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//some javascript below
+//update javascript
 
 window.onload = function(){
   var button = document.getElementById("button-yellow");
   button.addEventListener("click", function(event){
     prompt('What is your email?');
-    button.innerHTML = "Thanks for your email!"
+     button.innerHTML = "Thanks for your email!"
   });
 }
