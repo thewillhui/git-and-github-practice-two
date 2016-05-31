e3914f4410ec0799341f4b31c23469158f53a959
Will Hui <will@willhui.com>
2016-05-31 16:34:13 +0800

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+//some javascript below
+
+window.onload = function(){
+  var button = document.getElementById("button-yellow");
+  button.addEventListener("click", function(event){
+    prompt('What is your email?');
+    button.innerHTML = "Thanks for your email!"
+  });
+}
